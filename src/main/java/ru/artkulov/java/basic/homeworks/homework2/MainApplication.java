5d54410ee6605a80c835cbbde0e53daaf7756034
Artem Akulov <artkulov_ww@mail.ru>
2024-07-17 19:51:53 +0300

    Сделано домашнее задание №2 с заданиями без звездочки

---
+++ src/main/java/ru/artkulov/java/basic/homeworks/homework2/MainApplication.java
@@ -0,0 +1,60 @@
+package ru.artkulov.java.basic.homeworks.homework2;
+
+import java.util.Arrays;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        int[] array = {12, 5, 7, 9, 8, 6};
+
+    }
+
+    public static void writeString(int n, String str) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(str);
+        }
+    }
+
+    public static void sumArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 5)
+                sum += array[i];
+        }
+        System.out.println("Сумма элементов массива, значение которых больше 5 = " + sum);
+    }
+
+    public static void fillArray(int n, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = n;
+        }
+    }
+
+    public static void increaseTheNumber(int a, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] += a;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void whichHalfBigger(int[] array) {
+        int sumLeft = 0;
+        int sumRight = 0;
+        for (int i = 0; i < array.length / 2; i++) {
+            sumLeft += array[i];
+        }
+        for (int i = array.length / 2; i < array.length; i++) {
+            sumRight += array[i];
+        }
+        if (sumLeft > sumRight)
+            System.out.println("Сумма элементов левой половины массива больше " + sumLeft + " > " + sumRight);
+        else if (sumRight > sumLeft)
+            System.out.println("Сумма элементов правой половины массива больше " + sumRight + " > " + sumLeft);
+        else
+            System.out.println("Суммы элементов левой и правой половины равны " + sumRight + " = " + sumLeft);
+    }
+
+    // Задания под звездочкой
+
+
+
+}
