9d2bd24eb3b0dd2096d2e136d73224fce1647687
Artem Akulov <artkulov_ww@mail.ru>
2024-07-21 23:24:20 +0300

    Сделал два задания под звездочкой

--- src/main/java/ru/artkulov/java/basic/homeworks/homework2/MainApplication.java
+++ src/main/java/ru/artkulov/java/basic/homeworks/homework2/MainApplication.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class MainApplication {
     public static void main(String[] args) {
         int[] array = {12, 5, 7, 9, 8, 6};
-        sumArrays(new int[]{1, 2, 6, 7}, new int[]{5, 6, 8, 9, 2, 3, 6,}, new int[]{1, 6, 8, 0, 4});
+        int[] array1= {4,2,1,1,1,2,1,1,1,1};
+
     }
 
     public static void writeString(int n, String str) {
@@ -56,12 +57,45 @@ public class MainApplication {
     // Задания под звездочкой
 
     public static void sumArrays(int[]... arrays) {
-        int[] sum = new int[];
+        int size = 0;
+        for (int i = 0; i < arrays.length; i++) {
+            if (size < arrays[i].length) {
+                size = arrays[i].length;
+            }
+        }
+        int[] sum = new int[size];
         for (int i = 0; i < arrays.length; i++) {
             for (int j = 0; j < arrays[i].length; j++) {
-
-
+                sum[j] += arrays[i][j];
             }
+            System.out.println(Arrays.toString(arrays[i]));
+        }
+        System.out.println("Результат:\n" + Arrays.toString(sum));
+    }
+
+    public static void balanceLeftAndRight(int[] array) {
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < array.length; i++) {
+            left += array[i];
+            for (int j = i + 1; j < array.length; j++) {
+                right += array[j];
+            }
+            if (left==right){
+                for (int k = 0; k < i; k++) {
+                    System.out.print(array[k]+", ");
+                }
+                System.out.print(array[i]+", | ");
+                for (int n = i+1; n <array.length-1; n++) {
+                    System.out.print(array[n]+", ");
+                }
+                System.out.print(array[array.length-1]);
+                break;
+            }
+            else{
+                right=0;
+            }
+
 
 
         }
