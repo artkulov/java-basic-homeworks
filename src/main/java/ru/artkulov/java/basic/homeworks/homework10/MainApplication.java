553c89f7ad7386d46a3659686e502a5136a9db26
Artem Akulov <artkulov_ww@mail.ru>
2024-09-03 20:19:08 +0300

    10 Домашнее задание выполнено

---
+++ src/main/java/ru/artkulov/java/basic/homeworks/homework10/MainApplication.java
@@ -0,0 +1,22 @@
+package ru.artkulov.java.basic.homeworks.homework10;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        PhoneBook pB = new PhoneBook();
+        // заполняем справочник данными
+        pB.add("Корсунов", "+7 (911) 322-22-23");
+        pB.add("Иванов", "+7 (911) 444-44-44");
+        pB.add("Корсунов", "+7 (911) 322-22-23");
+        pB.add("Сидоров", "+7 (911) 555-55-55");
+        pB.add("Андреев", "+7 (911) 666-69-69");
+        pB.add("Корсунов", "+7 (911) 772-27-72");
+        pB.add("Иванов", "+7 (911) 802-08-02");
+        pB.add("Корсунов", "+7 (911) 999-09-90");
+        // запрашиваем результаты
+        pB.find("Корсунов");
+        pB.find("Иванов");
+        pB.find("Сидоров");
+        System.out.println(pB.containsPhoneNumber("+7 (911) 999-09-90"));
+        System.out.println(pB.getContacts());
+    }
+}
